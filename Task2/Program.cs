a00055812b5f1fe4085e76c4b554b749ccaa783e
Administrator <Administrator@DESKTOP-S2AOHHA>
2021-11-08 11:54:19 +0200

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int Num1, Num2;
+            Console.WriteLine(“Enter first number:”);
+            Num1 = Console.ReadLine();
+
+            Console.WriteLine(“Enter second number:”);
+            Num2 = Console.ReadLine();
+        }
+    }
+}
